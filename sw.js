1d2a22330cc2933deffa879a2d1d3446685250f2
anupon-bit <anupon@rachakhaopadpoo.com>
2026-09-24 22:41:53 +0700

    Bump PWA cache for faster bootstrap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='krua-flow-attendance-v4-6-fast-bridge';
+const CACHE='krua-flow-attendance-v4-7-fast-bootstrap';
 const SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
